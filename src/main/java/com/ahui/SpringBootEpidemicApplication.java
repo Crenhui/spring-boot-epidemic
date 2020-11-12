1cf8fda4a7b26f0e9226457e74ce353fe70b5f6b
Crenhui <2672523130@qq.com>
2020-11-12 13:36:05 +0800

    Initial commit

---
+++ src/main/java/com/ahui/SpringBootEpidemicApplication.java
@@ -0,0 +1,13 @@
+package com.ahui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEpidemicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEpidemicApplication.class, args);
+	}
+
+}
